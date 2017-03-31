9d78e5704ac334500eda6e7d7cd8182e37609a6e
Kien Nguyen T <kien.nguyen@asiantech.vn>
2017-03-31 10:06:25 +0700

    add vietnamese language

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+puts 'Create Department'
 10.times do |n|
   department = Department.find_or_create_by(name: "Phong ban #{n + 1}")
 end
 
+puts 'Create user'
 10.times do |n|
   user = User.create(email: "test+#{n+1}@gmail.com", password: '12345678', department_id: "#{n+1}".to_i)
 end
 
+puts 'Create Document'
 1000.times do |n|
   Document.create(title: "Title #{n+1}", content: "Content #{n+1}", department_id: rand(1..10))
 end
 
+puts 'Create DepartmentDocument'
 10000.times do |n|
   DepartmentDocument.create(document_id: rand(1..1000), department_id: rand(1..10))
 end
